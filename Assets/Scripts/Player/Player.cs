404b039f5552a2a77a5ef9d91a17e5da7a29b2f2
PedroFreitasFanatee <pedropifre@gmail.com>
2021-11-11 01:17:28 -0300

    Add Jump in prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,22 +6,47 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f,0);
 
-    public float speed;
+    public float speed = 7;
+
+    public float forceJump = 5;
 
     public void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleMoviment();
+        HandleJump();
+    }
+
+    private void HandleMoviment()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
-            myRigidBody.velocity = new Vector2(-speed,myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
         }
 
+        if(myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity -= friction; 
+        }
+
+        if (myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = Vector2.up * forceJump;
+        }
     }
 }
